ab45fa386d37dd82c5ecae833b9938aacfeca90a
Joseph C. DiMaggio <success1@gmail.com>
2018-03-18 21:20:39 -0400

    Added command line input

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+const yargs = require('yargs');
 const request = require('request');
 
-const googleGeocodeEndpoint = 'https://maps.googleapis.com/maps/api/geocode/json'
-const googleAPIKey = 'AIzaSyCF23ks0NGyu9epZM0uRJTLadZvsfrGMMU';
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string:true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+// console.log(argv);
+var encodedAddress = encodeURIComponent(argv.address);
+var googleGeocodeEndpoint = 'https://maps.googleapis.com/maps/api/geocode/json';
+var googleAPIKey = 'AIzaSyCF23ks0NGyu9epZM0uRJTLadZvsfrGMMU';
+var requestURL = `${googleGeocodeEndpoint}?address=${encodedAddress}&key=&{googleAPIKey}`;
+
 request ({
-    url: `${googleGeocodeEndpoint}?address=1301%20lombard%20street%20philadelphia&key=&{googleAPIKey}`,
+    url: requestURL,
     json: true
 }, (error, response, body) => {
     // console.log(JSON.stringify(response, undefined, 4));  // format JSON with indent = 4
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`Latitude: ${body.results[0].geometry.location.lat}, Longitude: ${body.results[0].geometry.location.lng}`);
 });
-
